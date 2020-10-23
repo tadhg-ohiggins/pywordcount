53369a0f424a22e8e6bb9736b7d9685f17504bee
Tadhg O'Higgins <tadhg@tadhg.com>
2020-10-23 15:12:58 -0700

    Main change: add a method for getting the base dir so it's easier to run it from somewhere that's not the CLI. Other: a bunch of formatting changes.

--- python/pywordcount/pywordcount_core.py
+++ python/pywordcount/pywordcount_core.py
@@ -11,41 +11,39 @@ import sys
 import os
 import codecs
 
+
 class PyWordCounter(object):
 
-    LINE_SEPS = (
-        "\r",
-        "\n"
-    )
+    LINE_SEPS = ("\r", "\n")
 
     WORD_SEPS = (
-        " ",        # space
-        "\n",       # linebreak
-        "\t",       # tab
-        "/",        # slash
-        "&",        # ampersand
-        '"',        # double quotation mark, straight
+        " ",  # space
+        "\n",  # linebreak
+        "\t",  # tab
+        "/",  # slash
+        "&",  # ampersand
+        '"',  # double quotation mark, straight
         u"\u201C",  # double quotation mark, left
         u"\u201D",  # double quotation mark, right
         u"\u2018",  # single quotation mark, left
         u"\u2013",  # en dash
         u"\u2014",  # em dash
         u"\xa0",  # non-breaking space
-        ">",        # greater than symbol
-        "<",        # less than symbol
-        "*",        # asterisk
-        "+",        # plus
-        "=",        # equals
-        "(",        # left parenthesis
-        ")",        # right parenthesis
-        "[",        # left bracket
-        "]",        # right bracket
-        "{",        # left curly bracket
-        "}",        # right curly bracket
-        "|",        # bar
-        ":",        # colon; URL handling normally makes this irrelevant
-        ";",        # semicolon
-        #Multichar separators:
+        ">",  # greater than symbol
+        "<",  # less than symbol
+        "*",  # asterisk
+        "+",  # plus
+        "=",  # equals
+        "(",  # left parenthesis
+        ")",  # right parenthesis
+        "[",  # left bracket
+        "]",  # right bracket
+        "{",  # left curly bracket
+        "}",  # right curly bracket
+        "|",  # bar
+        ":",  # colon; URL handling normally makes this irrelevant
+        ";",  # semicolon
+        # Multichar separators:
         "--",
         "--",
         "..",
@@ -53,19 +51,22 @@ class PyWordCounter(object):
     )
 
     IGNORE = (
-    #Not separators per se, but should not be treated as word content
-        "'",        # single quotation mark, straight
+        # Not separators per se, but should not be treated as word content
+        "'",  # single quotation mark, straight
         u"\u2019",  # single quotation mark, right
-        "-",        # hyphen
-        "#",        # hash mark
-        ".",        # period
-        "_",        # underscore
-        "`",        # backtick
-        "\\",        # backslash
+        "-",  # hyphen
+        "#",  # hash mark
+        ".",  # period
+        "_",  # underscore
+        "`",  # backtick
+        "\\",  # backslash
     )
 
     def __init__(self):
         self.processes = []
+
+    def get_base_dir(self):
+        return os.path.realpath(os.path.dirname(__file__))
 
     def cli_main(self):
         """
@@ -88,26 +89,30 @@ class PyWordCounter(object):
 
         """
         import configparser as ConfigParser
+
         plugindir = "%s/plugins/" % base_dir
         sys.path.insert(0, plugindir)
         cp = ConfigParser.ConfigParser()
         cp.read("%s/config.ini" % base_dir)
 
-        #Parse the config data so that we end up with a list of processes for
-        #each filetype, or None for those filetypes without specified processes
+        # Parse the config data so that we end up with a list of processes for
+        # each filetype, or None for those filetypes without specified processes
         types = [ft.strip() for ft in cp.get("filetypes", "types").split(",")]
         procs = dict([(t, None) for t in types] + cp.items("preprocesses"))
-        def tolist(v): return v and [i.strip() for i in v.split(",")] or v
+
+        def tolist(v):
+            return v and [i.strip() for i in v.split(",")] or v
+
         procs = dict([(k, tolist(procs[k])) for k in procs])
 
-        #Get a unique list of the processes and import them
+        # Get a unique list of the processes and import them
         proc_funcs = {}
         for proc in set(sum([v for v in procs.values() if v], [])):
             proc_funcs[proc] = __import__(
-                proc, globals(), locals(),
-                ["pywordcountplugin"]).pywordcountplugin
+                proc, globals(), locals(), ["pywordcountplugin"]
+            ).pywordcountplugin
 
-        #Replace the string names with the actual functions:
+        # Replace the string names with the actual functions:
         for ft in procs:
             procs[ft] = procs[ft] and [proc_funcs[p] for p in procs[ft]] or []
 
@@ -124,6 +129,7 @@ class PyWordCounter(object):
         Sets up the parser for handling command line options.
         """
         from optparse import OptionParser
+
         parser = OptionParser()
         parser.add_option(
             "-f",
@@ -133,31 +139,25 @@ class PyWordCounter(object):
             metavar="FILE",
             action="store",
         )
-        parser.add_option(
-            "-t",
-            "--type",
-            dest="type",
-            help="set TYPE",
-            metavar="TYPE"
-        )
+        parser.add_option("-t", "--type", dest="type", help="set TYPE", metavar="TYPE")
         return parser
 
     def cli_options(self, args, parser):
         options, args = parser.parse_args(args)
         opts = dict(vars(options))
-        #Split types by comma:
+        # Split types by comma:
         if opts.get("type", False):
             opts["type"] = [i.strip() for i in opts["type"].split(",")]
-        #Treat unparsed args as filenames:
+        # Treat unparsed args as filenames:
         if opts.get("file", False):
             opts["file"] = [opts["file"]]
             if len(args) > 1:
                 opts["file"] += args[1:]
 
-        #have to eliminate the empty keys
-        #See http://www.siafoo.net/article/52#dictionary-comprehensions
-        #return dict([k, v] for k, v in opts.iteritems() if v != None)
-        #The above doesn't work in Python 2.3, so:
+        # have to eliminate the empty keys
+        # See http://www.siafoo.net/article/52#dictionary-comprehensions
+        # return dict([k, v] for k, v in opts.iteritems() if v != None)
+        # The above doesn't work in Python 2.3, so:
 
         clean_opts = {}
         for k, v in opts.iteritems():
@@ -179,7 +179,9 @@ class PyWordCounter(object):
             self.files = [sys.stdin.read()]
             return
 
-        def read(f): return codecs.open(f, mode="r",encoding="utf-8").read()
+        def read(f):
+            return codecs.open(f, mode="r", encoding="utf-8").read()
+
         self.files = [read(f) for f in files]
 
     def count_text(self, text):
@@ -193,8 +195,9 @@ class PyWordCounter(object):
         return ["c: %s w: %s l: %s" % self.count_text(f) for f in self.files]
 
     def count_words(self, text):
+        def ors(l):
+            return r"|".join([re.escape(c) for c in l])
 
-        def ors(l): return r"|".join([re.escape(c) for c in l])
         def retext(text, chars, sub):
             return re.compile(ors(chars)).sub(sub, text)
 
@@ -205,6 +208,7 @@ class PyWordCounter(object):
         words = text.strip() and len(re.compile(r"[ ]+").split(text)) or 0
 
         return (words, lns)
+
 
 if __name__ == "__main__":
     wc = PyWordCounter()
